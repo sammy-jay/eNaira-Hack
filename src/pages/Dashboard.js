c4211a14102ad5898b36bf2b17091dd448d23eb2
p-smoke <jaydensammy0@gmail.com>
2022-08-07 00:29:35 +0000

    history completed

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -5,8 +5,8 @@ import vector4 from "../assets/Vector-4.png";
 const Dashboard = () => {
   const navigate = useNavigate()
   return (
-    <section className=" md:py-8 md:pt-[40px] md:px-6 bg-transparent min-h-screen flex flex-col justify-start items-center mx-auto space-y-[20px] max-w-[400px]">
-      <div className="bg-[rgb(28,189,92)] flex flex-col p-4 w-full rounded-br-[24px] rounded-bl-[24px] text-white ">
+    <section className="pb-[60px] md:py-8 md:pt-[40px] md:px-6 bg-transparent min-h-screen flex flex-col justify-start items-center mx-auto space-y-[20px] max-w-[400px]">
+      <div className="bg-[rgb(28,189,92)] flex flex-col p-[1.5rem] w-full rounded-br-[24px] rounded-bl-[24px] text-white ">
         <p className="text-[14px] uppercase font-thin text-gray-50">
           Welcome Back
         </p>
@@ -17,7 +17,7 @@ const Dashboard = () => {
             alt="vector4"
             className="h-[60px] object-contain absolute  right-[25%] opacity-20"
           />
-          
+
           <p className="text-[16px] font-thin text-gray-50">Your balance is</p>
           <div className="flex justify-between mt-3 items-center">
             <span className="text-[26px] font-bold">#213,304.45</span>
@@ -26,20 +26,57 @@ const Dashboard = () => {
         </div>
         {/* btns */}
         <div className="flex space-x-1 w-full">
-          <button className="text-[#0B7428] shadow bg-[#D2F2DE] rounded text-center py-2 flex-1" onClick={()=> navigate("/home/recieve")}>
+          <button
+            className="text-[#0B7428] shadow bg-[#D2F2DE] rounded text-center py-2 flex-1"
+            onClick={() => navigate("/home/recieve")}
+          >
             Recieve
           </button>
           <p className="bg-[rgba(28, 189, 92, 0.2)]"></p>
-          <button className="text-[rgba(245,69,69,1)] shadow bg-[#FDDADA] rounded text-center py-2 flex-1" onClick={()=> navigate("/home/transfer")}>
+          <button
+            className="text-[#f54545] shadow bg-[#FDDADA] rounded text-center py-2 flex-1"
+            onClick={() => navigate("/home/transfer")}
+          >
             Transfer
           </button>
         </div>
       </div>
-      <div className="w-full flex justify-between items-center px-2">
-        <p className="tex-[16px] font-semibold">Recent Transactions</p>
+      <div className="w-full flex justify-between items-center px-[1.5rem]">
+        <p className="text-[16px] font-semibold">Recent Transactions</p>
         <p className="text-[#828282] text-sm cursor-pointer">See All</p>
       </div>
-      <div className="w-full p-2 bg-white h-[270px] rounded-tr-[24px] rounded-tl-[24px] "></div>
+      <div className="w-full p-[1.5rem] pt-0 bg-white overflow-y-scroll max-h-[320px] rounded-tr-[24px] rounded-tl-[24px] ">
+        <p className="text-[14px] font-semibold my-3">7TH AUG 2022</p>
+
+        {[...new Array(3)].map((_, index) => (
+          <div
+            key={index}
+            className="flex justify-between space-x-3 items-center my-3"
+          >
+            <div className="w-[40px] h-[40px] rounded-full bg-green-200" />
+            <div className="flex-1">
+              <p className=" text-[12px] text-[#828282]">Tranferred from</p>
+              <p className=" text-[15px]">Blake Jason</p>
+            </div>
+            <p className="text-[#0B7428]">+ 500.00</p>
+          </div>
+        ))}
+        <p className="text-[14px] font-semibold my-3">5TH AUG 2022</p>
+
+        {[...new Array(3)].map((_, index) => (
+          <div
+            key={index}
+            className="flex justify-between space-x-2 items-center my-3"
+          >
+            <div className="w-[40px] h-[40px] rounded-full bg-green-200" />
+            <div className="flex-1 space-y-1">
+              <p className=" text-[12px] text-[#828282]">Tranferred to</p>
+              <p className=" text-[15px]">John Doe</p>
+            </div>
+            <p className="text-[#f54545] ">- 1500.00</p>
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
